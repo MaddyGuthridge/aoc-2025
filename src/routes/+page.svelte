c51a5835b7d14ed02f784bc0c16d76d0e9f923d3
Maddy Guthridge <hello@maddyguthridge.com>
2025-12-01 21:07:38 +1100

    Ok I give up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,11 @@
 
 <ul>
   {#each days as day (day)}
-    <li><a href={resolve(`/day01`)}>Day {day}</a></li>
+    <!--
+      This is really fugly to hard-code it to make it work in deployment, but I simply
+      do not have the energy to figure out how to make this work nicely when it isn't
+      at the root of the domain.
+    -->
+    <li><a href="/aoc-2025/day01">Day {day}</a></li>
   {/each}
 </ul>
